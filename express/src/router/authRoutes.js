fb0eefd6f5466164d6825e9d41ee70a5aff63baf
ChristianCoC <christianelmillo81@gmail.com>
2024-04-23 06:15:06 -0300

    Instalamos zod y agregamos validaciones al registro y login

--- express/src/router/authRoutes.js
+++ express/src/router/authRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { login, register, logout, profile } from "../controllers/authControllers.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validateMiddlewares.js";
+import { registerSchema, loginSchema } from "../schemas/authSchemas.js";
 
 const authRoutes = Router();
 
-authRoutes.post("/register", register);
-authRoutes.post("/login", login);
+authRoutes.post("/register", validateSchema(registerSchema), register);
+authRoutes.post("/login", validateSchema(loginSchema), login);
 authRoutes.post("/logout", logout);
 
 authRoutes.get("/profile", authRequired, profile);
